867baf82056ba3e1780e88fa8c8b2c438975128a
Johan Alexis Loaiza Culma <91573350+AlexisLoaiza125@users.noreply.github.com>
2026-05-25 15:01:06 -0500

    database

--- database/__init__.py
+++ database/__init__.py
@@ -5,3 +5,4 @@ def init_db():
     """Crea todas las tablas definidas en los modelos."""
     from models import usuario, ejercicio, rutina, registro_progreso  # noqa: F401
     Base.metadata.create_all(bind=engine)
+    print("✅ Base de datos inicializada.")
